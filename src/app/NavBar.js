802bd3d5d2d7d9c409499cad17f0ede669ac3d67
Alberto Cabrera Socorro <alberto@cabrerasocorro.es>
2023-02-20 20:21:38 +0000

    Breadcrumbs added

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,47 +1,68 @@
-import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import { useTranslation } from 'react-i18next'
-import { Link } from 'react-router-dom'
-import TranslateIcon from '@mui/icons-material/Translate'
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import { useTranslation } from "react-i18next";
+import { Link, useLocation } from "react-router-dom";
+import TranslateIcon from "@mui/icons-material/Translate";
 
 const styles = {
   navbar: {
-    backgroundColor: 'primary',
-    height: '10vh'
+    backgroundColor: "primary",
+    height: "10vh",
   },
   navbarItem: {
-    textDecoration: 'none',
+    textDecoration: "none",
     flexGrow: 1,
-    color: 'white'
-  }
-}
+    color: "white",
+  },
+};
 
-export default function NavBar () {
-  const { t, i18n } = useTranslation()
+export default function NavBar() {
+  const { t, i18n } = useTranslation();
+  const location = useLocation();
 
   const changeLanguage = () => {
-    i18n.language === 'en'
-      ? i18n.changeLanguage('es')
-      : i18n.changeLanguage('en')
-  }
+    i18n.language === "en"
+      ? i18n.changeLanguage("es")
+      : i18n.changeLanguage("en");
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar sx={styles.navbar} position='static'>
+      <AppBar sx={styles.navbar} position="static">
         <Toolbar>
-          <Typography
-            variant='h5'
-            component={Link}
-            sx={styles.navbarItem}
-            to={'/'}
-          >
-            {t('mainTitle')}
-          </Typography>
+          {location?.pathname == "/" ? (
+            <Typography
+              variant="h5"
+              component={Link}
+              to={"/"}
+              sx={styles.navbarItem}
+            >
+              {t("mainTitle")}
+            </Typography>
+          ) : (
+            <Breadcrumbs aria-label="breadcrumb" sx={styles.navbarItem}>
+              <Link underline="hover" color="inherit" href="/">
+                <Typography
+                  variant="h5"
+                  component={Link}
+                  to={"/"}
+                  sx={styles.navbarItem}
+                >
+                  {t("mainTitle")}
+                </Typography>
+              </Link>
+              <Typography color="text.primary" sx={styles.navbarItem}>
+                {location?.pathname.split("/").slice(-1)}
+              </Typography>
+            </Breadcrumbs>
+          )}
+
           <TranslateIcon onClick={changeLanguage} />
         </Toolbar>
       </AppBar>
     </Box>
-  )
+  );
 }
